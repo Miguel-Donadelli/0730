fbd9532a6e813dfadfe8fb6ab6cd74fb876bee57
Miguel-Donadelli <miguel.donadelli@etec.sp.gov.br>
2025-08-06 17:49:15 -0300

    oi

---
+++ teste.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css"> 
+</head>
+<body>
+   <h1>Sorteio Da Mega-Sena</h1>
+
+ <div class="numeros">
+       <div class="bola">1</div>
+
+ </div>
+ 
+     <form method="POST">
+        <button class="btn-sortear" type="submit">Sortear Novamente</button>
+     </form>
+</body>
+</html>
