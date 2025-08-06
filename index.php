655078103fc7a2a37d3a1ff26c305fe8888f62ef
Miguel-Donadelli <miguel.donadelli@etec.sp.gov.br>
2025-08-06 17:50:00 -0300

    aaa

--- index.php
+++ index.php
@@ -8,29 +8,29 @@
 <body>
     <h1>Sorteio de Números</h1>
     <?php
-        // 1. Captura e validação da quantidade
-        $quantidade = 6; // valor padrão
+
+        $quantidade = 6; 
         if (isset($_POST['quantidade'])) {
             $quantidade = (int)$_POST['quantidade'];
             if ($quantidade < 6) $quantidade = 6;
             if ($quantidade > 10) $quantidade = 10;
         }
 
-        // 2. Sorteio dos números
+
         $numeros = range(1, 60);
         shuffle($numeros);
         $resultado = array_slice($numeros, 0, $quantidade);
         sort($resultado);
     ?>
 
-    <!-- 3. Formulário para escolher a quantidade -->
+ 
     <form method="POST">
         <label for="quantidade">Quantidade de números (6 a 10):</label>
         <input type="number" id="quantidade" name="quantidade" min="6" max="10" value="<?php echo $quantidade; ?>">
         <button class="btn-sortear" type="submit">Sortear Novamente</button>
     </form>
 
-    <!-- 4. Exibição dos números sorteados -->
+
     <div class="numeros">
         <?php foreach($resultado as $numero): ?>
             <div class="bola"><?php echo $numero; ?></div>
